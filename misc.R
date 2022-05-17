23c0b94e2592b65695d3afca4a2a42dd9551d609
svpol <12798243+svpol@users.noreply.github.com>
2022-05-17 13:50:06 +0300

    lin_mod function is added.
    The cunction calculates linear regression coeffcients: intercept and slope.

--- misc.R
+++ misc.R
@@ -1,7 +1,16 @@
 # Task taken from: https://stepik.org/lesson/11508/step/5?unit=2531
-# Write corr.calc function which accept a data.frame with 2 numeric columns, calculates Pearson correlation coefficient and returns a vector of 2 values: correlation coefficient and p-value.
+# Write corr.calc function which accepts a data.frame with 2 numeric columns, calculates Pearson correlation coefficient and returns a vector of 2 values: correlation coefficient and p-value.
 
 corr.calc <- function(test_data){
   fit <- cor.test(test_data[[1]], test_data[[2]])
   return(c(fit$estimate, fit$p.value))
+}
+
+# Task taken from: https://stepik.org/lesson/11508/step/12?unit=2531
+# There is a data frame with 2 columns of numeric data. Buid linear regression where the first column is a dependent variable and the second one is independent.
+# Calculate the values of regression coefficients: intercept and slope.
+
+lin_mod <- function(test_data){
+  fit <- lm(test_data[[1]] ~ test_data[[2]])
+  return(fit$coefficients)
 }
